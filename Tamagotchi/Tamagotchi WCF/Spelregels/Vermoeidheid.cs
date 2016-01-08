86697497781d7b6e00abe5a703290c6613062390
nkop <nkop1@avans.nl>
2016-01-08 12:07:15 +0100

    push

---
+++ Tamagotchi/Tamagotchi WCF/Spelregels/Vermoeidheid.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Tamagotchi_WCF.Spelregels
+{
+    public class Vermoeidheid : ISpelregel
+    {
+        public Tamagotchi ExecuteSpelregel(Tamagotchi tmg)
+        {
+            TimeSpan Difference = tmg.LastAcces - DateTime.Now;
+            int HoursPassed = (int)Difference.TotalHours;
+            tmg.Sleep += (HoursPassed * 5);
+            tmg.LastAcces = DateTime.Now;
+            using (var context = new TmgContext())
+            {
+                context.Entry(tmg).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            return tmg;
+        }
+    }
+}
