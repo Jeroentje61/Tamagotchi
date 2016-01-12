8a23460b41973bf6f9e94658c4ae6333cea1938e
nkop <nkop1@avans.nl>
2016-01-12 18:52:41 +0100

    gayding

--- Tamagotchi/TamagotchiConsole/Program.cs
+++ Tamagotchi/TamagotchiConsole/Program.cs
@@ -27,9 +27,9 @@ namespace TamagotchiConsole
             else
             {
                 Console.WriteLine("Kies een van de volgende Tamagotchis door zijn/haar naam in te typen...");
-                if (service.GetTamagotchis() != null)
+                if (service.GetLivingTamagotchis() != null)
                 {
-                    foreach (TamagotchiService.Tamagotchi item in service.GetTamagotchis())
+                    foreach (TamagotchiService.Tamagotchi item in service.GetLivingTamagotchis())
                     {
                         Console.Write(item.Naam + ", ");
                     }
