975f7b83b91b18aa2e87cdbddf4cf585e24fb9a7
Jeroentje61 <jfpbreke@avans.nl>
2016-01-18 10:25:10 +0100

    Tests

--- Tamagotchi/ServiceTest/UnitTest1.cs
+++ Tamagotchi/ServiceTest/UnitTest1.cs
@@ -4,15 +4,20 @@ using Tamagotchi_WCF;
 using Moq;
 using System.Data.Entity;
 using System.Collections.Generic;
+using Tamagotchi_WCF.Spelregels;
+using Tamagotchi_WCF.Actions;
 
 namespace ServiceTest
 {
     [TestClass]
     public class UnitTest1
     {
+        
+        private Tamagotchi_WCF.Tamagotchi tamagotchi;
+        private Tamagotchi_WCF.TmgContext tmgcontext;
+
         public UnitTest1()
         {
-           
             //mock tamagotchis aanmaken
             IList<Tamagotchi> tamagotchis = new List<Tamagotchi>
             {
@@ -52,7 +57,12 @@ namespace ServiceTest
 
         }
         
-       
+       [TestInitialize]
+        public void Init()
+        {
+            tmgcontext = new TmgContext();
+            tamagotchi = new Tamagotchi();
+        }
         
         public Tamagotchi GetTamagotchi()
         {
@@ -72,41 +82,111 @@ namespace ServiceTest
         }
 
         [TestMethod]
-        public void GetTamagotchiTest()
+        public void EatTest()
         {
+            //Arrange
+            tamagotchi.Sleep = 100;
+            tamagotchi.Alive = false;
+            //Act
+            IAction i = new Eat();
+            //    "eat" = i.Act(tamagotchi);
+            //Assert
+            Assert.AreEqual(false, tamagotchi.Alive);
         }
 
         [TestMethod]
-        public void CreateTamagotchiTest()
+        public void SlaapTekortTest()
         {
             //Arrange
-            
-            string name = "BlaBla";
-            Tamagotchi tmg;
+            tamagotchi.Sleep = 100;
+            tamagotchi.Alive = false;
             //Act
-            //tmg = mocktamagotchi.CreateTamagotchi(name);
+            ISpelregel i = new Slaaptekort();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
             //Assert
-           // Assert.IsNotNull(tmg);
+            Assert.AreEqual(false, tamagotchi.Alive);
+        }
+        [TestMethod]
+        public void CrazyTest()
+        {
+            //Arrange
+            tamagotchi.Health = 100;
+            tamagotchi.Crazy = true;
+            //Act
+            ISpelregel i = new Crazy();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(100, tamagotchi.Health);
         }
 
         [TestMethod]
-        public void PerformActionTest()
+        public void HongerTest()
         {
             //Arrange
-            string actie = "sleep";
-            string result;
-            Tamagotchi tmgtest = GetTamagotchi();
-
+            tamagotchi.Hunger = 100;
+            tamagotchi.Alive = false;
             //Act
-            //result = service.PerformAction(actie, tmgtest);
-
+            ISpelregel i = new Honger();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
             //Assert
-            //Assert.AreEqual(result, "Je Tamagotchi is aan het slapen. Dit duurt 2 uur.");
+            Assert.AreEqual(false, tamagotchi.Alive);
         }
-
-        public void ChooseTamagotchiTest()
+        [TestMethod]
+        public void IsolatieTest()
         {
-
+            //Arrange
+            tamagotchi.Health = 100;
+            //Act
+            ISpelregel i = new Isolatie();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(0, tamagotchi.Health);
+        }
+        [TestMethod]
+        public void MunchiesTest()
+        {
+            //Arrange
+            tamagotchi.Boredom = 100;
+            tamagotchi.Munchies = true;
+            //Act
+            ISpelregel i = new Munchies();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(true, tamagotchi.Munchies);
+        }
+        [TestMethod]
+        public void TopatleetTest()
+        {
+            //Arrange
+            tamagotchi.Health = 0;
+            tamagotchi.TopAtleet = true;
+            //Act
+            ISpelregel i = new Topatleet();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(true, tamagotchi.TopAtleet);
+        }
+        [TestMethod]
+        public void VermoeidheidTest()
+        {
+            //Arrange
+            tamagotchi.Sleep = 100;
+            //Act
+            ISpelregel i = new Vermoeidheid();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(100, tamagotchi.Sleep);
+        }
+        [TestMethod]
+        public void VervelingTest()
+        {
+            //Arrange
+            tamagotchi.Boredom = 100;
+            //Act
+            ISpelregel i = new Verveling();
+            tamagotchi = i.ExecuteSpelregel(tamagotchi);
+            //Assert
+            Assert.AreEqual(100, tamagotchi.Boredom);
         }
     }
 }
